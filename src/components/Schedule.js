f53d75039715a9e4d86bb8a8938dee6fd5ca9d40
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-03 00:20:08 +0530

    schedule component created

---
+++ src/components/Schedule.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Schedule = () => {
+  return (
+    <div className='list'>
+      <p className='list-item'>Schedule:1</p>
+      <p className='list-item'>Schedule:2</p>
+      <p className='list-item'>Schedule:3</p>
+      <p className='list-item'>Schedule:4</p>
+    </div>
+  );
+};
+
+export default Schedule;
--- src/pages/climb/Climb.js
+++ src/pages/climb/Climb.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import mountainthree from '../../images/mountainthree.jpg';
+import Schedule from '../../components/Schedule';
 import './climb.css';
 
 const Climb = () => {
   return (
     <div className='climb-main-container'>
       <div className='climb-navbar-container'>
-        <div className='climb-number'>
-          <p className='the-two'>02</p>
+        <div className='climb-navbar-first-half'>
+          <div className='climb-number'>
+            <p className='the-two'>02</p>
+          </div>
+          <div className='climb-txt'>
+            <p className='the-txt'>CLIMB</p>
+            <div className='climb-blank'></div>
+          </div>
         </div>
-        <div className='climb-txt'>
-          <p className='the-txt'>CLIMB</p>
-          <div className='climb-blank'>B</div>
-        </div>
+
         <div className='climb-lorem-txt'>
           <p className='lorem-txt'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
@@ -34,7 +38,9 @@ const Climb = () => {
         <div
           style={{ backgroundImage: `url(${mountainthree})` }}
           className='climb-block'
-        ></div>
+        >
+          <Schedule />
+        </div>
       </div>
     </div>
   );
